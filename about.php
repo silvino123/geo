96bf3e8ca14242c3dbb658d96eaac56291bde8d8
silvino123 <36116136+silvino123@users.noreply.github.com>
2019-04-23 10:03:22 -0700

    vv
    v

---
+++ about.php
@@ -0,0 +1,226 @@
+        
+        
+        
+<!DOCTYPE html>     
+<html>      
+        
+<head>      
+    <title>Geocommsa-Quienes Somos</title>        
+    <meta name="viewport" content="width=device-width, initial-scale=1">        
+    <meta charset="utf-8">      
+    <meta name="Geocommsa" content="" />        
+        
+    <script>        
+        addEventListener("load", function() {       
+            setTimeout(hideURLbar, 0);      
+        }, false);      
+            
+        function hideURLbar() {     
+            window.scrollTo(0, 1);      
+        }       
+    </script>       
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />      
+    <link href="css/simpleLightbox.css" rel='stylesheet' type='text/css' />     
+    <link href="css/style.css" rel='stylesheet' type='text/css' />      
+    <link href="css/fontawesome-all.css" rel="stylesheet">      
+            
+</head>     
+            
+<body>      
+    <header>        
+    <?php include 'header.html'; ?>     
+    </header>       
+    <!--//header-->     
+    <div class="banner-inner-celular">      
+    </div>      
+    <ol class="breadcrumb">     
+        <li class="breadcrumb-item">        
+            <a href="index.php">Inicio</a>      
+        </li>       
+        <li class="breadcrumb-item active">Quienes Somos</li>     
+                  
+    </ol>       
+    <!--//banner-->     
+    <section class="banner-bottom">
+        <div class="container">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row inner-sec-wthree">
+
+                <div class="col-lg-5 bt-bottom-info" data-aos="fade-right">
+                    <h5>GEOCOMMSA</h5>
+                 <!-- <img src="images/geocomssa.gif" style="width: 300px;height: 80px" class="img-responsive"> -->
+                </div>
+                <div class="col-lg-7 bt-bottom-info" data-aos="fade-left">
+                    <p>Somos una empresa orgullosamente Sonorense que provee soluciones tecnológicas de comunicación y trasmisión de datos vía satelital, celular, dual y radio frecuencia. Todo ello, a la medida y necesidad de cada cliente, como pudiera ser el control y seguimiento de variables, registro de datos y comportamientos en tiempo real, automatización-integración, desarrollo y optimización de procesos, sensores, equipos y maquinaria. Ayudando en la reducción de costos, que es un factor determinante en el éxito y rentabilidad de la empresas.</p>
+                </div>
+            </div>
+            <div class="row inner-sec-wthree">
+                <div class="col-lg-4 bottom-sub-grid text-center aos-init aos-animate" data-aos="zoom-in">
+                    <div class="bt-icon">
+
+                        <span class="fas fa-trophy"></span>
+                    </div>
+
+                    <h4 class="sub-tittle"><b>MISIÓN</b></h4>
+                    <p>En GEOCOMMSA Buscamos ser una aliado de cada uno de nuestros clientes, desarrollando soluciones Tecnológicas personalizadas, a su medida y necesidades, usando como base principal la aplicaciones de la Telemetria (M2M) y el uso del IioT para la trasmisión de datos en tiempo real.</p>
+                    
+                </div>
+                <!-- /.col-lg-4 -->
+                <div class="col-lg-4 bottom-sub-grid text-center aos-init aos-animate" data-aos="zoom-in">
+                    <div class="bt-icon">
+                        <span class="far fa-thumbs-up"></span>
+                    </div>
+
+                    <h4 class="sub-tittle"><b>VISIÓN</b></h4>
+                    <p>En GEOCOMMSA siempre buscaremos ser un socio estratégico de nuestros clientes, proporcionando la tecnología y los recursos que conlleva la promesa del IioT, la Telemetria (M2M) de impulsar un mayor retorno de la inversión de nuestros clientes.</p>
+                    
+                </div>
+                <!-- /.col-lg-4 -->
+                
+                <!-- /.col-lg-4 -->
+            </div>
+        </div>
+        <!-- /.row -->
+    </section> 
+ <!--reviews_sec-->
+ <section class="reviews_sec" id="valores">
+        <h3 class="tittle cen"> Nuestros Valores</h3>
+        <div class="inner-sec-wthree">
+                <section >
+                        <div class="container">
+                            
+                            <div class="row inner-sec-wthree">
+                                <div class="col-lg-4 grid_info_main" data-aos="flip-left">
+                                    <div class="grid_info">
+                                        <div class="icon_info" style="opacity:0.7">
+                                           <span class="icon" >
+                                         <i class="fas fa-check-circle"></i>
+                                            
+                                           </span>
+                                            <h5 style="color:#ff6a00">Seguridad</h5>
+                                            <p>En todas nuestras actividades, la seguridad de todos los miembros del equipo, es nuestra prioridad número uno. Cada miembro de nuestro equipo debe pensar en regresar a sus hogares sanos y a salvo.</p>
+                                        </div>
+                                    </div>
+                                    <div class="grid_info second" style="opacity:0.7">
+                                        <div class="icon_info">
+                                            <span class="icon" >
+                                                <i class="fas fa-check-circle"></i>
+                                                   
+                                                  </span>
+                                            <h5 style="color:#ff6a00">Relaciones de trabajo</h5>
+                                            <p>Construimos relaciones sólidas con nuestros clientes, basadas en la honestidad, integridad, respeto y comunicación abierta. El Trabajo en asociación con nuestros clientes es crucial para el éxito. Siempre hay un camino para conseguir el trabajo bien hecho. Nuestro equipo esta preparado para enfrentar los obstáculos mecánicos y tecnológicos a través de la investigación y desarrollo de soluciones innovadoras y eficientes para cumplir los objetivos de nuestros clientes.
+                                                <br>
+                                                <br>
+                                                <br>
+                                                <br>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 grid_info_main mid" data-aos="flip-down">
+                                        <div class="grid_info">
+                                                <div class="icon_info" style="opacity:0.7">
+                                                    <span class="icon" >
+                                                        <i class="fas fa-check-circle"></i>
+                                                           
+                                                          </span>
+                                                    <h5 style="color:#ff6a00">Profesionales</h5>
+                                                    <p>En todas nuestras actividades, la seguridad de todos los miembros del equipo, es nuestra prioridad número uno. Cada miembro de nuestro equipo debe pensar en regresar a sus hogares sanos y a salvo.</p>
+                                                </div>
+                                            </div>
+                                            <div class="grid_info second">
+                                                    <div class="icon_info" style="opacity:0.7">
+                                                        <span class="icon" >
+                                                            <i class="fas fa-check-circle"></i>
+                                                               
+                                                              </span>
+                                                        <h5 style="color:#ff6a00">Medio ambiente</h5>
+                                                        <p>Respetamos el medio ambiente. tomamos todas las medidas prácticas para minimizar el impacto de nuestras actividades sobre el medio ambiente, tarea fundamental para el futuro de todos.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="grid_info second">
+                                                    <div class="icon_info"style="opacity:0.7">
+                                                        <span class="icon" >
+                                                            <i class="fas fa-check-circle"></i>
+                                                               
+                                                              </span>
+                                                        <h5 style="color:#ff6a00">Trabajo en equipo</h5>
+                                                        <p>Todos los colaboradores, deberán de está trabajando como un equipo más fuerte dia a dia.
+                                                            
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                </div>
+                                <div class="col-lg-4 grid_info_main" data-aos="flip-right">
+                                   
+                                    <div class="grid_info"style="opacity:0.7">
+                                        <div class="icon_info">
+                                            <span class="icon" >
+                                                <i class="fas fa-check-circle"></i>
+                                                   
+                                                  </span>
+                                            <h5 style="color:#ff6a00">Innovadores</h5>
+                                            <p>Somos solucionadores de problemas operativos de nuestros clientes con el constante desarrollo de herramientas Para aumentar la rentabilidad y la productividad de las empresas que nos contratan.</p>
+                                        </div>
+                                    </div>
+                                    <div class="grid_info second">
+                                            <div class="icon_info"style="opacity:0.7">
+                                                <span class="icon" >
+                                                    <i class="fas fa-check-circle"></i>
+                                                       
+                                                      </span>
+                                                <h5 style="color:#ff6a00">Autocritica</h5>
+                                                <p>Recibimos con beneplácito los comentarios de nuestros clientes y colaboradores, seguimos evolucionando y adaptándonos para satisfacer las necesidades de nuestros clientes e industria.</p>
+                                            </div>
+                                        </div>
+                                </div>
+                
+                
+                            </div>
+                        </div>
+                    </section>
+        </div>
+    </section>
+
+
+
+	        
+    <!--footer-->       
+    <?php include 'footer.html'; ?>     
+            
+    <!-- js -->     
+    <script src="js/jquery-2.2.3.min.js"></script>      
+    <!-- //js -->       
+    <!-- simpleLightbox -->     
+	<script src="js/simpleLightbox.js"></script>        
+	<script>        
+		$('.proj_gallery_grid a').simpleLightbox();     
+	</script>       
+	<!-- //simpleLightbox -->       
+    <!-- /js files -->      
+	<link href='css/aos.css' rel='stylesheet prefetch' type="text/css" media="all" />       
+	<link href='css/aos-animation.css' rel='stylesheet prefetch' type="text/css" media="all" />     
+	<script src='js/aos.js'></script>       
+	<script src="js/aosindex.js"></script>      
+	<!-- //js files -->     
+    <!-- start-smoth-scrolling -->      
+    <script src="js/move-top.js"></script>      
+    <script src="js/easing.js"></script>        
+    <script>        
+        jQuery(document).ready(function($) {        
+            $(".scroll").click(function(event) {        
+                event.preventDefault();     
+                $('html,body').animate({        
+                    scrollTop: $(this.hash).offset().top        
+                }, 900);        
+            });     
+        });     
+    </script>       
+        
+     <!-- /js -->       
+       <script src="js/bootstrap.js"></script>      
+    <!-- //js -->       
+</body>     
+            
+</html>     
---
+++ index.php
@@ -0,0 +1,245 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Geocommsa-Index</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="Geocommsa" content="" />
+
+    <script>
+        addEventListener("load", function() {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+
+    </script>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+    <link href="css/style.css" rel='stylesheet' type='text/css' />
+    <link href="css/simpleLightbox.css" rel='stylesheet' type='text/css' />
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+   
+</head>
+
+<body>
+    <header>
+        <?php include 'header.html'; ?>
+    </header>
+    <!--//header-->
+    <!--/banner-->
+    <div class="banner">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <div class="carousel-caption">
+                        <h3>TEXTO</h3>
+
+                    </div>
+                </div>
+                <div class="carousel-item item2">
+                    <div class="carousel-caption">
+                        <h3>TEXTO</h3>
+
+                    </div>
+                </div>
+                <div class="carousel-item item3">
+                    <div class="carousel-caption">
+                        <h3>TEXTO</h3>
+
+
+                    </div>
+                </div>
+               
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+        </div>
+    </div>
+   
+    <!--//banner-->
+    <section class="banner-bottom">
+    <div class="container">
+          <div class="row inner-sec-wthree">
+            <div class="col-lg-7 bt-bottom-info" data-aos="fade-right">
+              <h4 style="color:#ff6a00">GEOCOMMSA</h4>
+                 <!-- <img src="images/geocomssa.gif" style="width: 300px;height: 80px" class="img-responsive"> -->
+                 <br>
+                 <p>Somos una empresa orgullosamente Sonorense que provee soluciones tecnológicas de comunicación y trasmisión de datos vía satelital, celular, dual y radio frecuencia. Todo ello, a la medida y necesidad de cada cliente, como pudiera ser el control y seguimiento de variables, registro de datos y comportamientos en tiempo real, automatización-integración, desarrollo y optimización de procesos, sensores, equipos y maquinaria. Ayudando en la reducción de costos, que es un factor determinante en el éxito y rentabilidad de la empresas.</p>
+                 
+                </div>
+        <div class="col-lg-5 bt-bottom-info" data-aos="fade-left">
+        <img src="images/edificio.jpg" alt="" class="img-fluid">
+          
+       </div>
+         </div>
+    </div>
+    </section>
+    
+    <section class="services">
+        <div class="container">
+            <h3 class="tittle">Nuestras Soluciones Tecnológicas </h3>
+            <div class="row inner-sec-wthree">
+                <div class="col-lg-4 grid_info_main" data-aos="flip-left">
+                    <div class="grid_info">
+                        <div class="icon_info">
+                            <span class="icon">
+								<i class="far fa-check-circle"></i>
+							</span>
+                            <h5 style="color:#ff6a00">REDES</h5>
+                            <p>TEXTO</p>
+                        </div>
+                    </div>
+                    <div class="grid_info second">
+                        <div class="icon_info">
+                            <span class="icon">
+								<i class="far fa-check-circle"></i>
+							</span>
+                            <h5 style="color:#ff6a00">HARDWARE</h5>
+                            <p>TEXTO</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 grid_info_main mid" data-aos="flip-down">
+                    <img src="images/celular.jpeg" class="img-fluid" alt=" " style="width:380px;height:540px">
+                </div>
+                <div class="col-lg-4 grid_info_main" data-aos="flip-right">
+                    <div class="grid_info">
+                        <div class="icon_info">
+                            <span class="icon">
+								<i class="far fa-check-circle"></i>
+							</span>
+                            <h5 style="color:#ff6a00">SOLUCIONES</h5>
+                            <p>TEXTO</p>
+                        </div>
+                    </div>
+                    <div class="grid_info second">
+                        <div class="icon_info">
+                            <span class="icon">
+								<i class="far fa-check-circle"></i>
+							</span>
+                            <h5 style="color:#ff6a00">INDUSTRIA</h5>
+                            <p>TEXTO</p>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+   
+    <section class="stats_test container-fluid">
+    <h3 class="tittle">Nuestras Soluciones Tecnológicas </h3>
+    <br>
+<div class="row inner_stat">
+    <div class="col-lg-7 stats-left-info">
+        <div class="row">
+            <div class="col-md-6 stats_left counter_grid" data-aos="flip-down">
+            
+				<i class="far fa-check-circle"></i>
+			
+                <h4 style="color:#ff6a00">REDES</h4>
+                <p>TEXTO</p>
+            </div>
+            <div class="col-md-6 stats_left counter_grid" data-aos="flip-down">
+            <i class="far fa-check-circle"></i>
+                <h4 style="color:#ff6a00">HARDWARE</h4>
+                <p>TEXTO</p>
+            </div>
+            <div class="col-md-6 stats_left counter_grid" data-aos="flip-down">
+            <i class="far fa-check-circle"></i>
+                <h4 style="color:#ff6a00">SOLUCIONES</h4>
+                <p>TEXTO</p>
+            </div>
+            <div class="col-md-6 stats_left counter_grid" data-aos="flip-down">
+            <i class="far fa-check-circle"></i>
+                <h4 style="color:#ff6a00">INDUSTRIA</h4>
+                <p>TEXTO</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-5 stats-right-img" data-aos="fade-left">
+
+    </div>
+</div>
+</section>
+  <br>
+    
+    <!--footer-->
+    <?php include 'footer.html'; ?>
+    <!-- /footer -->
+    <script src="js/jquery-2.2.3.min.js"></script>
+ 
+    <script src="js/simpleLightbox.js"></script>
+    <script>
+        $('.proj_gallery_grid a').simpleLightbox();
+
+    </script>
+	<link href='css/aos.css' rel='stylesheet prefetch' type="text/css" media="all" />
+	<link href='css/aos-animation.css' rel='stylesheet prefetch' type="text/css" media="all" />
+	<script src='js/aos.js'></script>
+	<script src="js/aosindex.js"></script>
+    <script defer src="js/jquery.flexslider.js"></script>
+    <script>
+        $(window).load(function() {
+            $('.flexslider').flexslider({
+                animation: "slide",
+                start: function(slider) {
+                    $('body').removeClass('loading');
+                }
+            });
+        });
+
+    </script>
+
+    <!-- //flexSlider -->
+
+    <!-- stats -->
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.countup.js"></script>
+    <script>
+        $('.counter').countUp();
+
+    </script>
+    <!-- //stats -->
+
+    <!-- start-smoth-scrolling -->
+    <script src="js/move-top.js"></script>
+    <script src="js/easing.js"></script>
+    <script>
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event) {
+                event.preventDefault();
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 900);
+            });
+        });
+
+    </script>
+   
+    <!-- /js -->
+    <script src="js/bootstrap.js"></script>
+    <!-- //js -->
+
+
+</body>
+
+</html>
